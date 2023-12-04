5db3b4679502c9fba9d40c6836c82fa406b0d5dc
тест <nigayolga95@gmail.com>
2023-12-04 12:42:25 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
+import About from './About';
 import './App.css';
+import Home from './Home';
+import {
+
+  BrowserRouter as Router,
+
+  Routes,
+
+  Route,
+
+  Link
+
+} from "react-router-dom";
+
+import Shop from './Shop';
+import Team from './Team';
+import Contact from './Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+      <Router>
+      <nav className='nav'>
+        <Link className='link' to="/">First page</Link>
+        <Link className='link' to="/about">About</Link>
+        <Link  className='link' to="/home">Home</Link>
+        <Link className='link' to="/shop">Shop</Link>
+        <Link className='link' to="/contact">Contact</Link>
+        <Link className='link' to="/team">Team</Link>
+      </nav>
+      
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/team" element={<Team />} />
+        </Routes>
+      </Router>
+ 
   );
 }
 
---
+++ src/Team.js
@@ -0,0 +1,9 @@
+function Team() {
+    return (
+        <div>
+            Our team
+        </div>
+    )
+}
+
+export default Team
